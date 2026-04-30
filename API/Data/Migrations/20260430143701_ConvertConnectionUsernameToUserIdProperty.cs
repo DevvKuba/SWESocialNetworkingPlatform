7fba5cd4172d4c10dd9e77c5b9d0e6120cc8a234
DevvKuba <jrfitnessinbox@gmail.com>
2026-04-30 15:44:00 +0100

    Create and apply migrations for Connection entity property change

---
+++ API/Data/Migrations/20260430143701_ConvertConnectionUsernameToUserIdProperty.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ConvertConnectionUsernameToUserIdProperty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Username",
+                table: "Connections");
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Connections",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Connections");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Username",
+                table: "Connections",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
