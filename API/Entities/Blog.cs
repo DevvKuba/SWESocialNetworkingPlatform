d6c413712ceb9ede2c7a4389f6c0263da65d7037
DevvKuba <jrfitnessinbox@gmail.com>
2026-05-02 21:58:58 +0100

    Declare Blog entity

---
+++ API/Entities/Blog.cs
@@ -0,0 +1,20 @@
+﻿namespace API.Entities
+{
+    public class Blog
+    {
+        public int Id { get; set; }
+
+        public required string SenderUsername { get; set; }
+
+        public required string Description { get; set; }
+
+        public DateTime PublishedAt { get; private set; }
+
+        public bool IsDeleted { get; set; } = false;
+
+        public int SenderId { get; set; }
+
+        public AppUser Sender { get; set; } = null!;
+
+    }
+}
